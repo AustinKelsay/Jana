6afee962a84034c6e3ee41e919e893f4022fc4f6
austinkelsay <austinkelsay@yahoo.com>
2020-10-22 13:28:39 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1 className="header">Jana Kelsay</h1>
+      
+      <div className="top-windows">
+        <div className="window">
+          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.np2BFmZCfeu1IxZnI1ogjQHaE8%26pid%3DApi&f=1" />
+        </div>
+        <div className="window">
+          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.np2BFmZCfeu1IxZnI1ogjQHaE8%26pid%3DApi&f=1" />
+        </div>
+      </div>
+
+      <div className="bottom-windows">
+        <div className="window">
+          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.np2BFmZCfeu1IxZnI1ogjQHaE8%26pid%3DApi&f=1" />
+        </div>
+        <div className="window">
+          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.np2BFmZCfeu1IxZnI1ogjQHaE8%26pid%3DApi&f=1" />
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
